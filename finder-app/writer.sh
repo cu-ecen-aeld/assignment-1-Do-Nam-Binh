c4762fb24afd0d4f36e7fc163dab6b2b5b184dd2
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2026-05-07 20:18:02 +0700

    assignment1-10

---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [$# -lt 2]; then
+    exit 1
+fi
+
+writefile=$1
+writestring=$2
+
+if ! mkdir -p "$(dirname "$writefile")"; then
+    echo "Error: could not create directory" >&2
+    exit 1
+fi
+
+if ! echo "$writestring" > "$writefile"; then
+    echo "Error: could not write to file" >&2
+    exit 1
+fi
